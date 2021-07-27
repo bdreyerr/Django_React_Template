3f14db3439f8aa6098771c4b50e7b11bf850fb6c
Ben Dreyer <38509839+bdreyerr@users.noreply.github.com>
2021-07-27 14:09:10 -0600

    Update settings.py

--- djreact/settings.py
+++ djreact/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z9ra!bc*dh2j43kdm(#r01-wtlhe87u_fxm*c=*qxy$kzm%@hq'
+SECRET_KEY = 'ADD SECRET KEY HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
